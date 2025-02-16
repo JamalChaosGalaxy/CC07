a26ab0b6647949c047eec5882d9fab27eb84deea
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-02-16 03:42:33 +0000

    Feb 16, 2025, 3:42 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -19,18 +19,5 @@
 
 import random
 
-beta = []
 
-for _ in range(100):
-    num = random.randint(1, 1000)
-    beta.append(num)
-
-print (num[: 10])
-
-skibidi = 234
-
-if num not in beta:
-    print("Yes, its in")
-else:
-    print("no, its not in")
 
