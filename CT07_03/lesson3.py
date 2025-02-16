c08796013862672c6bf7d91c27b8054e05b55a77
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-02-16 03:00:28 +0000

    Feb 16, 2025, 3:00 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -1,2 +1,45 @@
-print("Hello from lesson 3")
+# # print("Hello from lesson 3")
+# # savings = 0
+
+# # while savings < 100:
+# #     daily_savings = float(input("how much did you save today?"))
+# #     savings += daily_savings
+# #     # savings = savings + daily_savings
+
+# #     if savings >= 100:
+# #         print("Congratulations you asian cheapskate you saved 100 dollars fuiyohh")
+# #         break
+
+# # while savings <= 100:
+# #     daily_savings = float(input("how much did you save today?"))
+# #     savings += daily_savings
+# #     #  savings = savings + daily_savings
+
+# # print("Congratulations you asian cheapskate you saved 100 dollars fuiyohh")
+
+# import random
+
+# total_Qns = 15
+# lives = 3
+
+# for i in range(total_Qns):
+#     num1 = random.randint(2,20)
+#     num2 = random.randint(2,20)
+#     correct_ans = num1 * num2
+
+#     while lives>0:
+#         answer = int(input(f"what is {num1} x {num2} ?"))
+
+#         if answer == correct_ans:
+#             print(f"Correct!")
+#             break;
+#         else:
+#             lives -= 1
+#             print("Wrong try again")
+
+#         if lives == 0:
+#             print("Go and see MS Tan for Remedial!")
+
+# if lives > 0:
+#     print("Congrats you have completed the quiz!")
 
--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -1,2 +1,66 @@
-print("Hello from lesson 4")
+# print("Hello from lesson 4")
+
+# solarsystem = [
+#     "Mercury",
+#     "Venus",
+#     "Earth",
+#     "Mars",
+#     "Jupiter",
+#     "Saturn",
+#     "Uranus",
+#     "Neptune"
+# ]
+
+# print(solarsystem[3])
+# solarsystem[3] = "Skibidi"
+# print(solarsystem[3])
+# print(len(solarsystem))
+
+# solarsystem.insert(3, "hi")
+# solarsystem.append("end")
+
+# solarsystem.insert(3, "lalaland")
+# solarsystem.append("Pluto")
+
+# # solarsystem.pop(5)
+# # del(solarsystem[5])
+
+# for counter in range(len(solarsystem)):
+#     print(solarsystem[counter])
+
+#     if counter == 2:
+#         print("this is my home")
+#     if counter == 4:
+#         print("conquered this")
+#     if counter == 3:
+#         print("created this")
+
+# countries = []
+# i = 0
+
+# while True:
+#     country = input("What is a country you would like to visit?")
+
+#     if country == "End":
+#         break
+
+#     countries.append(country)
+#     i += 1 
+
+# for country in countries:
+#         print(f"I would like to visit {country}")
+
+menu = []
+i = 0
+
+while True:
+    food = input("What would you like to add to the menu?")
+    if food == "end":
+        break
+
+    menu.append(food)
+    i += 1 
+
+for food in menu:
+        print(f"I would like to add {food} to the menu!")
 
