7361614dddabad8b19644382fdd8a38f60ce9fd4
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-03-02 03:06:29 +0000

    Mar 2, 2025, 3:06 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,30 +1,32 @@
-# # # print("Hello from lesson 7")
+# # # # print("Hello from lesson 7")
 
-# # students = []
-# # student1 = ["Eliot", "34528375", "Basketball"]
-# # student2 = ["James", "23453665", "Badminton"]
-# # student3 = ["Peter", "73465345", "Football"]
+# # # students = []
+# # # student1 = ["Eliot", "34528375", "Basketball"]
+# # # student2 = ["James", "23453665", "Badminton"]
+# # # student3 = ["Peter", "73465345", "Football"]
 
-# # students.append(student1)
-# # students.append(student2)
-# # students.append(student3)
+# # # students.append(student1)
+# # # students.append(student2)
+# # # students.append(student3)
 
-# # for student in students:
-# #     name, phonenum, cca = student
-# #     print("Name: " + name)
-# #     print("Phone number: " + phonenum)
-# #     print("CCA:" + cca)
+# # # for student in students:
+# # #     name, phonenum, cca = student
+# # #     print("Name: " + name)
+# # #     print("Phone number: " + phonenum)
+# # #     print("CCA:" + cca)
 
-# list1 = ["Apple", "Banana", "Cherry"]
-# list2 = ["Durian", "Elderberry", "Figs"]
+# # list1 = ["Apple", "Banana", "Cherry"]
+# # list2 = ["Durian", "Elderberry", "Figs"]
 
-# fruits = list1 + list2
-# print(fruits)
+# # fruits = list1 + list2
+# # print(fruits)
+
+# list1 = [3.20, 2.65, 1.75]
+# list2 = [6.15, 5.45, 4.20]
+
+# total = list1 + list2
+# print("Before sort: ", total)
+# sorted_price = sorted(total)
+# print("After sort: " ,sorted_price)
 
 list1 = [3.20, 2.65, 1.75]
-list2 = [6.15, 5.45, 4.20]
-
-total = list1 + list2
-print("Before sort: ", total)
-sorted_price = sorted(total)
-print("After sort: " ,sorted_price)
