b80daf6e2da5a6671ca7f4f8d3d81b194d607446
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-03-08 08:53:17 +0000

    Mar 8, 2025, 8:53 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,2 +1,59 @@
-print("Hello from lesson 2")
+# # # # print("Hello from lesson 3")
+
+# # i = 1
+
+# # while i < 3:
+# #     1 = input ("How many colors are in the rainbow")
+# #     if 1 == 7:
+# #         print("Correct")
+# #         2 = input("How much is 4 + 4")
+# #     else:
+# #         print("Incorrect")
+# #     if 2 == 8:
+# #         print("Correct")
+# #         3 = input ("How much is 2 + 2")
+# #     else:
+# #         print("Incorrect")
+# #     if 3 == 4:
+# #         print("Correct you finished the test")
+# #         break
+# #     else:
+# #         print("Incorrect")
+
+# score = 0
+# answer = input("What is the color of the orange")
+# while(answer != "orange"):
+#     print("Wrong answer try again noob")
+#     answer = input("What is the color of the orange")
+
+# print("Correct")
+# score = score + 1
+
+# answer = input("What is the color of the sun")
+# while(answer != "white"):
+#     print("Wrong answer try again noob")
+#     answer = input("What is the color of the sun")
+
+# print("Correct")
+# score = score + 1
+
+# answer = input("What is the color of an apple")
+# while(answer != "red"):
+#     print("Wrong answer try again noob")
+#     answer = input("What is the color of an apple")
+
+# print("Correct")
+# score = score + 1
+
+import time
+
+time = int (input("How many minutes do you want to study for?"))
+time = time * 60
+
+while ( time != 0):
+    print ("seconds")
+    time = time - 1
+    time.sleep(1)
+
+print("You suck at maths! A- HAIYAAA")
 
