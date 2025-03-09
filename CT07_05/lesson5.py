f813109648550688d0339e22f34a3b1adbe54804
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-03-09 00:19:29 +0000

    Mar 9, 2025, 12:19 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -90,7 +90,7 @@ powers = [
 ]
 
 index = pokemons.index("Machamp")
-print(pokemons[powers.index(max(powers))])
+print = 
 
 # pokemon1 = random.choice(pokemons)
 # pokemon2 = random.choice(pokemons)
