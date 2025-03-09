d0a3fd62b3ec0e73991838f4fdf9da8aaff06bb2
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-03-09 03:35:16 +0000

    Mar 9, 2025, 3:35 AM

--- CT07_MidSem/Practical.py
+++ CT07_MidSem/Practical.py
@@ -1,12 +1,14 @@
 import random
 health = 100;
 
-
+i = 0
 print(f"Hero starts on his adventure with Health: ", health)
 while health > 0:
     damage = random.randint(1,15)
     health -= damage
     print("After finishing monsters, his health is now", health)
     i += 1
+
+
     
     
