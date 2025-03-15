12f64a989a1fbccf81e0402096a3b3db2edec8b9
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-03-15 06:11:58 +0000

    Mar 15, 2025, 6:11 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -2,6 +2,6 @@
 # isalpha = check if all the characters in a variable are 
 # isalnum = only nums and alphabet letters and not special character
 # isupper = checks if the letters are uppercase
-# islower = checks is
+# islower = checks if the letters are lowercase
 # and, or , not
 
