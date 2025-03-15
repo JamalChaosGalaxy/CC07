30cf78f2b59aade23553ade11a8dd6a3c6525db4
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-03-15 06:11:06 +0000

    Mar 15, 2025, 6:11 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,4 +1,4 @@
-# isdigit
+# isdigit = checks if all the characters in a 
 # isalpha = check if all the characters in a variable are 
 # isalnum = only nums and alphabet letters and not special character
 # isupper = checks if the letters are uppercase
