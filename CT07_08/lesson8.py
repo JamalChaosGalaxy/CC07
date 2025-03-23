96ef0c16e4143ab6cd78479f1de4fbf39700bdcd
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-03-23 03:12:07 +0000

    Mar 23, 2025, 3:12 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -70,7 +70,13 @@
 # print(word[::-1])
 # print(f"Output: ",is_palindrome)
 
-word = input("Input: ")
+# word = input("Input: ")
+
+# is_palindrome = word == word[::-1]
+# print(f"Output: ",is_palindrome)
+
+while True:
+    word = input("Input: ")
 
 is_palindrome = word == word[::-1]
 print(f"Output: ",is_palindrome)
