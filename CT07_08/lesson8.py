042e547c8b505a1d40e6aebc69f404b1374aac3e
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-03-23 03:03:45 +0000

    Mar 23, 2025, 3:03 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -64,16 +64,3 @@
 
 # print("burgers"[::-1])
 
-def is_palindrome(word):
-    # Remove spaces and convert to lowercase for case-insensitive comparison
-    word = word.replace(" ", "").lower()
-    
-    # Check if the word is equal to its reverse
-    if word == word[::-1]:
-        return "Yes"
-    else:
-        return "No"
-
-# Test the function
-word = input("Enter a word: ")
-print(is_palindrome(word))
