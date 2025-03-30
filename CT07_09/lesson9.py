afcd35d44de003075bc770d8b7813e7cfce2308e
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-03-30 04:05:00 +0000

    Mar 30, 2025, 4:05 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -81,7 +81,7 @@ Sally.seth(0)
 Sally.shape("turtle")
 Sally.color("red")
 Sally.goto(0, -250)
-Sally.write("Sally", align="center", font=(""))
+Sally.write("Sally", align="center", font=("Ariel",20))
 
 # pen.speed(10)
 # pen.seth(0)
