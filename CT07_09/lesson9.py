e163ba4bb7c10c4e042d959078154f91cfe6f17f
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-03-30 03:09:08 +0000

    Mar 30, 2025, 3:09 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,14 +1,16 @@
-def is_palindrome(word):
-    # Remove spaces and convert to lowercase for case-insensitive comparison
-    word = word.replace(" ", "").lower()
+# def is_palindrome(word):
+#     # Remove spaces and convert to lowercase for case-insensitive comparison
+#     word = word.replace(" ", "").lower()
     
-    # Check if the word is equal to its reverse
-    if word == word[::-1]:
-        return "Yes"
-    else:
-        return "No"
+#     # Check if the word is equal to its reverse
+#     if word == word[::-1]:
+#         return "Yes"
+#     else:
+#         return "No"
 
-# Test the function
-word = input("Enter a word: ")
-print(is_palindrome(word))
+# # Test the function
+# word = input("Enter a word: ")
+# print(is_palindrome(word))
 
+
+# 
