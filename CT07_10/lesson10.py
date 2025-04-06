42166532a538058dd936713ef28a9fe0c144d085
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-06 03:53:54 +0000

    Apr 6, 2025, 3:53 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -1,13 +1,13 @@
+# 
 var1 = 200
 
-def f():
-    var1 = 500
-    global var2 
-    var2 = 300
-    print("var1: " + str(var1))
-    print("var2: " + str(var2))
+# def f():
+#     var1 = 500
+#     global var2 
+#     var2 = 300
+#     print("var1: " + str(var1))
+#     print("var2: " + str(var2))
 
-f()
-
+# f()
     
 
