a53996bc11fa4d5ca396ea411489e01dc01c3f4e
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-13 04:31:20 +0000

    Apr 13, 2025, 4:31 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -185,7 +185,6 @@ move = ["rock", "paper", "scissors"]
 import random
 def generate_comp_move():
     comp_move = int(random.randint(move))
-    return comp_move
 
 player_move = str(input("What is your chosen move?"))
 
