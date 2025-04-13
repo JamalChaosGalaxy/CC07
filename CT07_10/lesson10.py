8c82b8fe44fe9d72bb5b01213b71ca306bcd774f
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-13 04:05:15 +0000

    Apr 13, 2025, 4:05 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -122,8 +122,8 @@
 
 # turtle.mainloop()
 
-def whatsappMe(phonenum):
-    return "Whatsapp me at https://wa.me/" + phonenum
+# def whatsappMe(phonenum):
+#     return "Whatsapp me at https://wa.me/" + phonenum
 
-phonenum = input("Enter number: ")
-print(whatsappMe(phonenum))
+# phonenum = input("Enter number: ")
+# print(whatsappMe(phonenum))
