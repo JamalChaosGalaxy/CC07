9f3600531fe8eeece73846fe25c5d1e70a14b7f3
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-20 04:04:44 +0000

    Apr 20, 2025, 4:04 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -15,7 +15,7 @@ def print_board(board):
             if cell != " ":
                 print(" " + str(cell) + " ", end="")
 
-print(initialise_board)
-
+print(initialise_board())
+print_board(bo)
 
 
