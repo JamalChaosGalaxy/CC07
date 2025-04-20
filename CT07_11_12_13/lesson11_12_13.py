9166ef4031ad09280df691f6193e5f05f2a6cc4d
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-20 04:26:11 +0000

    Apr 20, 2025, 4:26 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -6,24 +6,24 @@ def initialise_board():
             row.append(' ')
         board.append(row)
     return board
-print(initialise_board())        
-# def print_board(board):
-#     print("\nBoard Layout: ")
-#     cell_number = 1
-#     for row in board:
-#         for cell in row:
-#             if cell != '':
-#                 print(" " + str(cell) + " ", end="")
-#             else:
-#                 print(" " + str(cell_number) + " ", end="")
-#             if cell_number % 3 != 0:
-#                 print("|", end="")
-#             cell_number += 1
-#         if cell_number <= 9:
-#             print("\n----------")
-#     print("\n")    
+print(initialise_board)        
+def print_board(board):
+    print("\nBoard Layout: ")
+    cell_number = 1
+    for row in board:
+        for cell in row:
+            if cell != '':
+                print(" " + str(cell) + " ", end="")
+            else:
+                print(" " + str(cell_number) + " ", end="")
+            if cell_number % 3 != 0:
+                print("|", end="")
+            cell_number += 1
+        if cell_number <= 9:
+            print("\n----------")
+    print("\n")    
         
 
-# print_board(board)
+print_board(board)
 
 
