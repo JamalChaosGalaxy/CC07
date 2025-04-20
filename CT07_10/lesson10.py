7656085ad338ce397eb405707b4f7f555e436efa
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-20 03:20:34 +0000

    Apr 20, 2025, 3:20 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -213,7 +213,7 @@ while True:
 
     comp_move = generate_comp_move
 
-    print(f"Computer's move: comp_move")
+    print(f"Computer's move: {comp_move}")
     print(f"Player's move: {player_move}")
 
     play_again = input("Do you want to play again? (y/n): ").lower()
