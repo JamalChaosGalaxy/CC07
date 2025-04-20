79098763399bb531de0bb58f0ea89c3d447a1205
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-20 04:24:57 +0000

    Apr 20, 2025, 4:24 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -8,7 +8,7 @@ def initialise_board():
     return board
         
 def print_board(board):
-    print("/nBoard Layout: ")
+    print("\nBoard Layout: ")
     cell_number = 1
     for row in board:
         for cell in row:
