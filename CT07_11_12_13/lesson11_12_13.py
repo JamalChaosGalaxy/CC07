00050af22c702ba621de309293e218992a7d233f
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-27 04:16:01 +0000

    Apr 27, 2025, 4:16 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -4,6 +4,10 @@ win_conditions = [
     [board[1][0], board[1][1], board[1][2]],
     [board[2][0], board[2][1], board[2][2]],
     #Vertical 
+    [board[0][0], board[0][1], board[0][2]],
+    [board[1][0], board[1][1], board[1][2]],
+    [board[2][0], board[2][1], board[2][2]],
+    
 ]
 
 def initialise_board():
