b5125e4940b03cf717054d315e12dcb8951fb922
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-27 04:15:20 +0000

    Apr 27, 2025, 4:15 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -1,7 +1,7 @@
 win_conditions = [
     # Horizontal
     [board[0][0], board[0][1], board[0][2]],
-    [board[0][0], board[0][1], board[0][2]],
+    [board[1][0], board[][1], board[0][2]],
     [board[0][0], board[0][1], board[0][2]],
     #Vertical 
 ]
