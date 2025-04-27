c2c27488e0e904514f098bf0fa7a375817c217f9
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-27 03:50:54 +0000

    Apr 27, 2025, 3:50 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -30,7 +30,7 @@ def get_player_move(board):
     move = int(move_input) - 1
     row = move // 3
     col = move % 3
-    board[[row][col]] = "x"
+    board[row][col] = "x"
 
 board = initialise_board()
 while True:
