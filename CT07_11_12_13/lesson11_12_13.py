ca18ad1f4f791e40d7e712944752497be7058176
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-27 04:15:10 +0000

    Apr 27, 2025, 4:15 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -2,7 +2,8 @@ win_conditions = [
     # Horizontal
     [board[0][0], board[0][1], board[0][2]],
     [board[0][0], board[0][1], board[0][2]],
-    [board[0][0], board[0][1], board[0][2]]
+    [board[0][0], board[0][1], board[0][2]],
+    #Vertical 
 ]
 
 def initialise_board():
