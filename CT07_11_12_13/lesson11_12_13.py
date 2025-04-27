443bb6fe909f8532f47ed640e3b6b44600007cb2
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-04-27 03:13:28 +0000

    Apr 27, 2025, 3:13 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -25,7 +25,7 @@ def print_board(board):
     print("\n")    
         
 def get_player_move(board):
-    move_input = input("")
+    move_input = input("Pick a number from 1 to 9:")
 
 print_board(initialise_board())
 
