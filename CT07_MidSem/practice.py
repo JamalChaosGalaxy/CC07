895dca04c294c8ce227405b0174934703b1ec7f2
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-01 10:55:32 +0000

    May 1, 2025, 10:55 AM

--- CT07_MidSem/practice.py
+++ CT07_MidSem/practice.py
@@ -1,15 +1,6 @@
 Regiplate = input("What is your registration plate?")
 Skibidi = Regiplate.split(",")
 
-numbers = 0
-aplha = 0
-for letter in Skibidi:
-    if letter.isdigit:
-        numbers += 1
-    if letter.isalpha:
-        alpha += 1
 
-if aplha > 4 or numbers > 4:
-    print("Car registration is invalid")
-else:
-    print("It is valid for checking")
+
+
