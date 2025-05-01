d8e6d430193e1db328c99a46d17f1080f5e6d201
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-01 10:08:50 +0000

    May 1, 2025, 10:08 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -6,30 +6,30 @@
 # and, or , not
 
 
-# while True:
-#     password = input("Please input your password: ")
-#     if len(password) >= 8 and password.isupper == False and password.islower == False and password.isalpha == False and password.isdigit == False and password.isalnum == True:
-#         print("Password is valid")
-#         break
-#     else:
-#         print("Password is invalid")
+while True:
+    password = input("Please input your password: ")
+    if len(password) >= 8 and password.isupper == False and password.islower == False and password.isalpha == False and password.isdigit == False and password.isalnum == True:
+        print("Password is valid")
+        break
+    else:
+        print("Password is invalid")
 
-# is8_char_long = False
-# has_upper = False
-# has_lower = False
-# has_num = False
-# only_alnum = False
+is8_char_long = False
+has_upper = False
+has_lower = False
+has_num = False
+only_alnum = False
 
-# password = input("Please enter your password: ")
-# has_upper = not(password.islower())
-# has_lower = not(password.isupper())
-# has_num = not(password.isalpha())
-# has_alpha = not(password.isalnum())
+password = input("Please enter your password: ")
+has_upper = not(password.islower())
+has_lower = not(password.isupper())
+has_num = not(password.isalpha())
+has_alpha = not(password.isalnum())
 
-# if (password.isalnum() and len(password) >= 8 and has_upper and has_lower and has_num):
-#     print("Password valid")
-# else:
-#     print("Password invalid")
+if (password.isalnum() and len(password) >= 8 and has_upper and has_lower and has_num):
+    print("Password valid")
+else:
+    print("Password invalid")
 
 # sentence = input("What is your sentence?")
 # mocked_sentence = ""
