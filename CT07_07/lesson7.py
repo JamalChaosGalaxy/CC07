bbdaede33c580535510e41ddc6e04ac35595afe1
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-01 10:06:17 +0000

    May 1, 2025, 10:06 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -29,13 +29,13 @@
 # sorted_price = sorted(total)
 # print("After sort: " ,sorted_price)
 
-fruits = ["Apple", "Banana", "Cherry", "Durian", "Elderberry", "Figs"]
-index = 3
+# fruits = ["Apple", "Banana", "Cherry", "Durian", "Elderberry", "Figs"]
+# index = 3
 
-left = fruits[:index]
-right = fruits[index:]
-print(left)
-print(right)
+# left = fruits[:index]
+# right = fruits[index:]
+# print(left)
+# print(right)
 # print(fruits)
 # print(len(fruits))
 
