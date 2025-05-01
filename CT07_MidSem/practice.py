a5bba24e24ee72cc6b3ef238a56648794d837abc
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-01 10:51:18 +0000

    May 1, 2025, 10:51 AM

--- CT07_MidSem/practice.py
+++ CT07_MidSem/practice.py
@@ -1,2 +1,13 @@
 Regiplate = input("What is your registration plate?")
 Skibidi = Regiplate.split(",")
+
+numbers = 0
+aplha = 0
+for letter in Skibidi:
+    if letter.isdigit:
+        numbers += 1
+    if letter.isalpha:
+        alpha += 1
+
+if aplha > 4 or numbers > 4:
+    print("")
