27aae8dab8864e1336c1c7e2fc75be2f1fc596cd
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-11 03:20:18 +0000

    May 11, 2025, 3:20 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -2,3 +2,4 @@ import pygame
 # If you try to import a library and it becomes an error, then in the terminal, you must input "pip install (library name)"
 
 pygame.init()#Initialize Pygame
+screen_width = 
