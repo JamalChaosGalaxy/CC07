80300ebeabc177e14f2e53f125d5c3ee123fdd26
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-11 03:32:41 +0000

    May 11, 2025, 3:32 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -15,7 +15,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-pygame.quit
+pygame.quit()
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong game")
