893f8a2ccc486c09381658b867f496e3d4d30c7f
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-11 03:17:03 +0000

    May 11, 2025, 3:17 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -1,2 +1,2 @@
 import pygame
-# If you try to import a library and it becomes an error, then in the terminal, you must input "pip"
+# If you try to import a library and it becomes an error, then in the terminal, you must input "pip install (library name)"
