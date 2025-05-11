bf3f4b8b0c39ca35dc832608a6e55e3dc6d3c108
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-11 03:21:47 +0000

    May 11, 2025, 3:21 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -2,6 +2,8 @@ import pygame
 # If you try to import a library and it becomes an error, then in the terminal, you must input "pip install (library name)"
 
 pygame.init()#Initialize Pygame
+
+# 
 screen_width = 100
 screen_height = 100
 screen = pygame.display.set_mode((screen_width, screen_height))
