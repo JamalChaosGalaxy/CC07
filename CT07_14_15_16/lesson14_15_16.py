f0d094d50a4ee9c4b741ad4be86a1bf5053de4a5
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-11 04:19:24 +0000

    May 11, 2025, 4:19 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -48,6 +48,7 @@ while running:
     
     pygame.draw.rect(screen, white, (paddle1_x, paddle1_y, paddle_width, paddle_height))
     pygame.draw.rect(screen, white, (paddle2_x, paddle2_y, paddle_width, paddle_height))
+    pygame.draw.circle(screen,)
     pygame.display.flip()
 
 pygame.quit()
