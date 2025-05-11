78620bf0eb580934642671af2cd024bfc9dd45c7
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-11 04:00:00 +0000

    May 11, 2025, 4:00 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -30,8 +30,8 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_w] and paddle1_y > 0:
-        paddle_
+    if keys[pygame.K_W] and paddle1_y > 0:
+        paddle_y = 
     
     pygame.draw.rect(screen, white, (paddle1_x, paddle1_y, paddle_width, paddle_height))
     pygame.draw.rect(screen, white, (paddle2_x, paddle2_y, paddle_width, paddle_height))
