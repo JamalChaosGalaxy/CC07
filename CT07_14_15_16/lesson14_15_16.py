92d32f7217f4203a33ca24e4051f7d75ee83b601
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-11 03:25:23 +0000

    May 11, 2025, 3:25 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -7,5 +7,5 @@ pygame.init()#Initialize Pygame
 screen_width = 100
 screen_height = 100
 screen = pygame.display.set_mode((screen_width, screen_height))
-
+# Siaplys the name of the game
 pygame.display.set_caption("Pong game")
