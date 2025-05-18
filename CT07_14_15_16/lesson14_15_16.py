2a3896e377b65601989b9db351ece23c39dabf87
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-18 03:36:32 +0000

    May 18, 2025, 3:36 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -61,7 +61,7 @@ while running:
 
     paddle1_box = pygame.Rect(paddle1_x, paddle1_y, paddle_width, paddle_height)
 
-    if ball_x <= paddle1_box.right + ball_radius and ball_y >= paddle1_box:
+    if ball_x <= paddle1_box.right + ball_radius and ball_y >= paddle1_box.top and ball:
         ball_dx *= -1
     
     pygame.draw.rect(screen, white, (paddle1_x, paddle1_y, paddle_width, paddle_height))
