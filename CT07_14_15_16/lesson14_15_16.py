80968d449b0d7b014e61875f3b3dd659b3362abd
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-18 04:05:17 +0000

    May 18, 2025, 4:05 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -72,7 +72,7 @@ while running:
     
     pygame.draw.rect(screen, white, (paddle1_x, paddle1_y, paddle_width, paddle_height))
     pygame.draw.rect(screen, white, (paddle2_x, paddle2_y, paddle_width, paddle_height))
-    pygame.draw.circle(screen, white, (ball_x, ball_y), ball_radius)
+    # pygame.draw.circle(screen, white, (ball_x, ball_y), ball_radius)
     pygame.display.flip()
 
     screen.blit(background_image, (0, 0))
