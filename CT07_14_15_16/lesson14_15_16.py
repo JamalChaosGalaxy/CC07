31104c4f80d2def1d2847ab2d2486df4fac2ad46
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-18 04:28:41 +0000

    May 18, 2025, 4:28 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -4,7 +4,7 @@ from pygame.transform import rotate
 
 pygame.init()#Initialize Pygame
 
-score_font
+score_font = pygame.font.font()
 # Creates the main window for your pygame 
 screen_width = 800
 screen_height = 600
