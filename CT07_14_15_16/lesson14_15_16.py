86f2c375ed1af7c062cac81295b38e89a6084b5a
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-18 03:36:02 +0000

    May 18, 2025, 3:36 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -59,7 +59,10 @@ while running:
     if ball_x  <= 0 or ball_x >= screen_width:
         ball_dx *= -1
 
-    paddle_box = pygame.Rect(paddle1_x, paddle1_y, paddle_width, paddle_height)
+    paddle1_box = pygame.Rect(paddle1_x, paddle1_y, paddle_width, paddle_height)
+
+    if ball_x <= paddle1_box.right + ball_radius and ball_y >= paddle1_box:
+        ball_dx 
     
     pygame.draw.rect(screen, white, (paddle1_x, paddle1_y, paddle_width, paddle_height))
     pygame.draw.rect(screen, white, (paddle2_x, paddle2_y, paddle_width, paddle_height))
