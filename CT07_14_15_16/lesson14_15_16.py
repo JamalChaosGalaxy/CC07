cdebeb2ebe5fcfe68576e0aad53d0b55d583408a
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-05-18 04:21:27 +0000

    May 18, 2025, 4:21 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -61,8 +61,8 @@ while running:
     if ball_y <= 0 or ball_y >= screen_height:
         ball_dy *= -1
 
-    if ball_x  <= 0 or ball_x >= screen_width:
-        ball_dx *= -1
+    # if ball_x  <= 0 or ball_x >= screen_width:
+    #     ball_dx *= -1
 
     paddle1_box = pygame.Rect(paddle1_x, paddle1_y, paddle_width, paddle_height)
     paddle2_box = pygame.Rect(paddle2_x, paddle2_y, paddle_width, paddle_height)
